9716ae1ae7f3b5144dba237e2616c7430471bd41
benson momodu <benson.isaac.momodu@gmail.com>
2019-08-03 20:07:06 +0100

    modifiling the game app and creation of one player system

--- App/Script.js
+++ App/Script.js
@@ -1,8 +1,221 @@
+var eventBus = new Vue ()
+
+Vue.component("chose-game-type", {
+    template: `
+        <div>
+            <button v-show="!chosen" @click="choseOnePlayer">Single Player</button>
+            <button v-show="!chosen" @click="choseTwoPlayer ">Two Player</button>
+        </div>
+        
+    `,
+    props: {
+        chosen: {
+            type: Boolean,
+            // required: true
+        }
+    },
+    methods: {
+        choseOnePlayer(){
+            this.$emit('chose-one-player')
+        },
+
+        choseTwoPlayer(){
+            this.$emit('chose-two-player')
+        },
+    }
+})
+
+
+Vue.component("single-player-form", {
+    template: `
+        <div>
+            <form v-show="!gameStarted" @submit.prevent="submitForm">
+                <label>Name</label>
+                <input v-model="name">
+            </form>
+            <one-player v-show="gameStarted"></one-player>
+        </div>
+    `, 
+    data () {
+        return {
+            name: '',
+            gameStarted: false
+        }
+    },
+
+    methods: {
+        submitForm () {
+            let PlayerName =  this.name
+            this.gameStarted = true
+            eventBus.$emit('SinglePlayerSubmitted', PlayerName)
+            this.name = null
+        }
+    }
+})
+
+Vue.component("two-player-form", {
+    template: `
+        <div>
+            <form v-show="!gameStarted" @submit.prevent="submitForm">
+                <label>Name</label>
+                <input v-model="playerOne">
+                <input v-model="playerTwo">
+                <input type="submit" value="Start game">
+            </form>
+            <two-player v-show="gameStarted"></two-player>
+        </div>
+    `, 
+    data () {
+        return {
+            playerOne: '',
+            playerTwo: '',
+            gameStarted: false
+        }
+    },
+
+    methods: {
+        submitForm () {
+            let players =  {
+                playerOne: this.playerOne,
+                playerTwo: this.playerTwo
+            }
+            this.gameStarted = true
+            eventBus.$emit('twoPlayerSubmitted', players)
+            this.playerOne = null
+            this.playerTwo = null
+        }
+    }
+})
+
 Vue.component("one-player",{
     template: `
-        <div>one player</div>
-    
-    `
+        <div class="app">  
+            <div class="card player">
+                <div class="name">
+                    <p>{{playerName}}<span v-if="playerPlying">.</span></p>
+                </div> 
+                <div class="ScoreBox">
+                    <p> Score:<span>{{playerScore}}</span></p>
+                </div>
+                <div class="actioBtn">
+                    <button :disabled="!playerPlying" type="submit"> Playing</button>
+                </div>
+            </div>
+            <div class="centered ">
+                <div class="diceIcon">
+                    <div>
+                        <img :src="dice1Image" alt="">
+                        <img :src="dice2Image" alt="">
+                    </div>
+                </div>
+
+                <div class="rollBtn">
+                    <button :disabled="gameEnded"  @click="rollDice" type="submit">Roll Dice</button>
+                    <button @click="restart" type="submit">restart</button>
+                    <p>Winner is: {{winner}}</p>
+                    
+                </div>
+            </div>
+            <div class="card computer">
+                <div class="name">
+                <p>{{computer}}<span v-if="computerPlying">.</span></p>
+                </div> 
+                <div class="ScoreBox">
+                    <p> Score: <span>{{dealerScore}}</span></p>
+                </div>
+                <div class="actioBtn">
+                    <button :class="{inActive: !computerPlying}" :disabled="!computerPlying" type="submit"> Playing</button>
+                </div>
+            </div>
+        </div>
+    `,
+    data () {
+        return {
+            playerName: '',
+            computer: 'Dealer',
+            playerScore: 0,
+            dealerScore: 0,
+            playerPlying: false,
+            computerPlying: false,
+            playerCount: 0,
+            computerCount: 0,
+            totalDice: 0,
+            dice1: 0,
+            dice2: 0,
+            dice1Image: "https://via.placeholder.com/50x50",
+            dice2Image: "https://via.placeholder.com/50x50",
+            roll: 0
+        }
+    },
+    methods: {
+        restart(){
+            this.playerScore = 0
+            this.dealerScore = 0
+            this.playerCount = 0
+            this.computerCount = 0
+            this.dice1Image = "https://via.placeholder.com/50x50"
+            this.dice2Image = "https://via.placeholder.com/50x50"
+            this.playerPlying = true
+            this.computerPlying = false
+        },
+
+
+        gameStarted (){
+            this.playerPlying = false
+        },
+        rollDice(){
+            this.dice1 = Math.floor(Math.random() * 6) +1
+            this.dice2 = Math.floor(Math.random() * 6) +1
+            this.dice1Image = `./images/dice-${this.dice1}.png`
+            this.dice2Image = `./images/dice-${this.dice2}.png`
+            // this.totalDice += this.dice1 + this.dice2;
+            // this.roll += 1
+            if(this.player1Plying){
+                this.playerCount +=1;
+                this.playerScore += this.dice1 + this.dice2
+                this.playerPlying = false
+                this.computerPlying = true
+            }else{
+                this.computerCount +=1;
+                this.dealerScore += this.dice1 + this.dice2
+                this.playerPlying = true
+                this.computerPlying = false
+            }
+            
+        }  
+    },
+
+    computed: {
+        winner () {
+            if(this.playerCount === 3 && this.computerCount === 3){
+                if(this.playerScore > this.dealerScore){
+                    return `${this.playerName} You win`
+                }else{
+                    return `${this.computer} You win`
+                }
+            }
+        },
+        gameEnded(){
+            if(this.playerCount === 3 && this.computerCount === 3){
+                return true
+            }else {
+                return false
+            }
+        }
+    },
+
+    mounted(){
+        this.gameStarted()
+        // console.log(this.name)
+        // if(this.playerPlying = false){
+        //     this.rollDice()
+        //     this.playerPlying = true
+        // }
+        eventBus.$on('SinglePlayerSubmitted', PlayerName => {
+            this.playerName = PlayerName 
+            // console.log(this.name)
+        })
+    }
 })
 
 
@@ -14,8 +227,7 @@ Vue.component("two-player", {
         <div class="app">  
             <div class="card player">
                 <div class="name">
-                    <p v-if="player1Plying">{{player1}}<span>.</span></p>
-                    <p v-else>{{player1}}</p>
+                    <p>{{players[0].playerOne}}<span v-if="player1Plying">.</span></p>
                 </div> 
                 <div class="ScoreBox">
                     <p> Score:<span>{{playerScore}}</span></p>
@@ -41,8 +253,7 @@ Vue.component("two-player", {
             </div>
             <div class="card computer">
                 <div class="name">
-                <p v-if="player2Plying">{{player2}}<span>.</span></p>
-                <p v-else>{{player2}}</p>
+                <p>{{players[0].playerTwo}}<span v-if="player2Plying">.</span></p>
                 </div> 
                 <div class="ScoreBox">
                     <p> Score: <span>{{dealerScore}}</span></p>
@@ -56,22 +267,31 @@ Vue.component("two-player", {
     
     data () {
         return {
-            player1: "benson",
-            player2: "shola",
+            players: [],
             playerScore: 0,
             dealerScore: 0,
             player1Plying: false,
             player2Plying: false,
             player1Count: 0,
             player2Count: 0,
+            totalDice: 0,
             dice1: 0,
             dice2: 0,
             dice1Image: "https://via.placeholder.com/50x50",
-            dice2Image: "https://via.placeholder.com/50x50"
+            dice2Image: "https://via.placeholder.com/50x50",
+            roll: 0
         }
     },
 
     methods: {
+        player1Take(){
+
+        },
+
+
+        player2Take(){
+
+        },
 
         restart(){
             this.playerScore = 0
@@ -93,6 +313,8 @@ Vue.component("two-player", {
             this.dice2 = Math.floor(Math.random() * 6) +1
             this.dice1Image = `./images/dice-${this.dice1}.png`
             this.dice2Image = `./images/dice-${this.dice2}.png`
+            // this.totalDice += this.dice1 + this.dice2;
+            // this.roll += 1
             if(this.player1Plying){
                 this.player1Count +=1;
                 this.playerScore += this.dice1 + this.dice2
@@ -126,6 +348,10 @@ Vue.component("two-player", {
         }
     },
     mounted(){
+        eventBus.$on('twoPlayerSubmitted', players => {
+            this.players.push(players)
+            console.log(this.players)
+        })
         this.gameStarted();
     }, 
     
@@ -135,9 +361,20 @@ var app = new Vue ({
     el: "#app",
 
     data: {
-
-        
+        singlePlayer: false,
+        twoPlayer: false,
+        gameTypeChosen: false     
     },
+    methods: {
+        handleSinglePlayerChosen () {
+            this.singlePlayer = true
+            this.gameTypeChosen = true
+        },
+        handleTwoPlayerChosen () {
+            this.twoPlayer = true
+            this.gameTypeChosen = true
+        }
+    }
         
 });
 
